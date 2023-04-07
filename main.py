09ea8fc699d1ee7f056f134d94964f40a5cb7f5e
natalia.bekarevich <fgerthtj@gmail.com>
2023-04-07 17:19:51 +0300

    efgndhjbj

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import telebot
 from bot_functions import *
 
 
-#6055095778:AAEkGWH4Tzw6q7OJuRCZSObIqQR3MuUV5-Q
-#f9c9614a1fb07bc22079e558848f4ec8
+
 
 if __name__ == '__main__':  # если запускается ИМЕННО ЭТОТ файл
     bot.polling()  # опрашивать сервер
